975200217a433b2e66397badd10429ec7b487369
adnan1175 <138349081+adnan1175@users.noreply.github.com>
2023-07-03 10:19:28 +0600

    Update browser.sh

--- browser.sh
+++ browser.sh
@@ -1,7 +1,9 @@
 r="\033[1;31m"
  w="\033[0m"
-echo "1 - install firefox"
-echo "2 - install chromium"
+clear
+echo "${r}which browse do you want${w}"
+echo "1 -  firefox"
+echo "2 -  chromium"
 echo "3 - install both"
 echo "4 - Exit"
 echo "Please choose one of the following options:" &&read choice
